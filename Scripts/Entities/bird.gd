d7ef0b367591a227291a67152dca5bdad16ce513
Nanocirs <nanocirs@gmail.com>
2024-01-31 01:07:19 +0100

    Added ray gun mechanics.

--- Scripts/Entities/bird.gd
+++ Scripts/Entities/bird.gd
@@ -5,10 +5,13 @@ signal on_bird_heat_changed;
 @export var max_fire_units : int = 100;
 @export var heat_decay : int = 2;
 @export var heat_decay_period : float = 1.0;
+@export var time_to_start_cooling : float = 2.0;
 
 var current_fire_units : int = 0;
-var timer_heat_decay = 0.0;
 var is_alive = true;
+
+var timer_heat_decay = 0.0;
+var timer_cooling = 0.0;
 
 
 func _ready():
@@ -18,10 +21,12 @@ func _ready():
 func _process(delta):
 	if is_alive:
 		timer_heat_decay += delta;
-		
-		if timer_heat_decay >= heat_decay_period:
-			timer_heat_decay = 0.0;
-			add_fire(-heat_decay);
+		timer_cooling += delta;
+
+		if timer_cooling >= time_to_start_cooling:
+			if timer_heat_decay >= heat_decay_period:
+				timer_heat_decay = 0.0;
+				add_fire(-heat_decay);
 
 		if current_fire_units >= max_fire_units:
 			kill();
@@ -29,7 +34,15 @@ func _process(delta):
 
 func add_fire(fire_units : int):
 	current_fire_units += fire_units;
-	on_bird_heat_changed.emit(fire_units);
+	
+	if current_fire_units < 0:
+		current_fire_units = 0;
+	else:
+		on_bird_heat_changed.emit(fire_units);
+		
+	$SubViewport/ProgressBar.value = current_fire_units;
+
+	timer_cooling = 0.0;
 
 
 func kill():
--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -1,12 +1,38 @@
 extends CharacterBody3D
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-@export var speed: float = 5;
+@export_group("Player settings")
+@export var speed: float = 5.0;
 @export var sensitivity : float = 2.0;
+
+@export_group("Gun settings")
+@export var rayDistance : float = 50.0;
+@export var fireUnitsPerTick : int = 1;
+@export var timeBetweenTicks : float = 0.1;
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity");
+
+var isFiring : bool = false;
+var timerGun : float = 0.0;
 
 func _init():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED;
+
+
+func _input(event):
+	if Input.is_action_just_pressed("fire"):
+		isFiring = true;
+	elif Input.is_action_just_released("fire"):
+		isFiring = false;
+
+
+func _process(delta):
+	timerGun += delta;
+	
+	if isFiring and timerGun >= timeBetweenTicks:
+		timerGun = 0.0;
+		detect_entity_in_sight();
+
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -14,6 +40,7 @@ func _physics_process(delta):
 	var direction = get_direction();
 	apply_velocity(direction);
 	move_and_slide();
+
 
 func apply_velocity(direction: Vector3) -> void:
 	if direction:
@@ -23,11 +50,22 @@ func apply_velocity(direction: Vector3) -> void:
 		velocity.x = move_toward(velocity.x, 0, speed);
 		velocity.z = move_toward(velocity.z, 0, speed);
 
+
 func get_direction() -> Vector3:
 	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down");
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized() * sensitivity;
 	return direction;
 
+
 func apply_gravity(gravity: float) -> void:
 	if not is_on_floor():
 		velocity.y -= gravity;
+
+
+func detect_entity_in_sight() -> void:
+	var ray = PhysicsRayQueryParameters3D.create($PlayerCamera.global_position, $PlayerCamera.global_position - $PlayerCamera.global_transform.basis.z * rayDistance);
+	var collision = get_world_3d().direct_space_state.intersect_ray(ray);
+	
+	if collision:
+		if collision.collider.is_in_group("bird"):
+			collision.collider.get_parent().add_fire(fireUnitsPerTick);
