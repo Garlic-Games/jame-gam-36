8109c6e78d4c6aa49b05808c75599c47671403c2
bocdagla <tomeu1m1a1@gmail.com>
2024-01-31 16:24:25 +0100

    Fixed weapon switch

--- Scripts/Player/WeaponChange.gd
+++ Scripts/Player/WeaponChange.gd
@@ -11,30 +11,31 @@ enum Weapon {
 @export var animation_duration: float = 3;
 
 var current_weapon: Weapon;
-var is_animating: bool;
+var is_animating: bool = false;
 
 func _ready():
-	current_weapon = Weapon.RAYGUN;
-	shotgun.deactivate();
-	raygun.activate();
+	toogle(Weapon.SHOTGUN);
 
 func _input(event):
 	if event.is_action_pressed("scroll") && !is_animating:
 		print("Weapon changed");
-		match current_weapon:
-			Weapon.SHOTGUN:
-				switchWeapon(raygun, shotgun);
-				current_weapon = Weapon.RAYGUN;
-			Weapon.RAYGUN:
-				switchWeapon(shotgun, raygun);
-				current_weapon = Weapon.SHOTGUN;
+		toogle(current_weapon);
 
+
+func toogle(current):
+	match current:
+		Weapon.SHOTGUN:
+			switchWeapon(shotgun, raygun);
+			current_weapon = Weapon.RAYGUN;
+		Weapon.RAYGUN:
+			switchWeapon(raygun, shotgun);
+			current_weapon = Weapon.SHOTGUN;
 
 func switchWeapon(equiped_weapon, next_weapon):
 	is_animating = true;
 	var tween = create_tween();
+	tween.tween_property(equiped_weapon, "position:y", -1, animation_duration/2);
 	tween.tween_callback(equiped_weapon.deactivate);
-	tween.tween_property(equiped_weapon, "position:y", -0.7, animation_duration/2);
 	var chain = tween.chain();
 	chain.tween_callback(next_weapon.activate);
 	chain.tween_property(next_weapon, "position:y", -0.6, animation_duration/2);
