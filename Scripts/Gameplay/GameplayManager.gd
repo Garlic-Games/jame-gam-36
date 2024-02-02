b2ffb7fa143440d4142cde24e909c931497788fe
Nanocirs <nanocirs@gmail.com>
2024-02-02 16:23:12 +0100

    Added ending song

--- Scripts/Gameplay/GameplayManager.gd
+++ Scripts/Gameplay/GameplayManager.gd
@@ -1,6 +1,8 @@
 extends Node3D
 class_name GameplayManager;
 
+@export var mainMusic : AudioStreamPlayer = null;
+@export var endMusic : AudioStreamPlayer = null;
 @export var gameOverMenu: GameOverManager;
 @export var heat : Node = null;
 @export var portal : Node3D = null;
@@ -14,11 +16,17 @@ func _ready():
 	
 	var tween_transition = get_tree().create_tween();
 	tween_transition.tween_property(fade_rectangle, "color:a", 0.0, 2.5);
+	
+	mainMusic.play();
 
 
 func EndGame():
 	GameStateMachine.changeState(GameStateMachine.GAME_STATE.GAME_OVER);
-
+	mainMusic.stop();
+	endMusic.play();
+	var tween_music = get_tree().create_tween();
+	tween_music.tween_property(endMusic, "volume_db", -10.0, 1.5);
+	
 
 func Restart():
 	GameStateMachine.changeState(GameStateMachine.GAME_STATE.PLAYING);
