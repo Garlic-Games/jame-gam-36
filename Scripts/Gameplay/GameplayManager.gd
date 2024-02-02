fc5f9dd7a722089f62674984903dbdbf83413351
Nanocirs <nanocirs@gmail.com>
2024-02-02 18:59:59 +0100

    added credits

--- Scripts/Gameplay/GameplayManager.gd
+++ Scripts/Gameplay/GameplayManager.gd
@@ -21,8 +21,6 @@ func _ready():
 	tween_transition.tween_property(fade_rectangle, "color:a", 0.0, 2.5);
 	
 	mainMusic.play();
-	
-	#tutorial.spawn_info(TutorialInfo.InformationState.FIRST);
 
 
 func EndGame():
